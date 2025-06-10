e0db8598798b608bdcfc949dc2084af77342b2ef
FauzanAmirudin <fauzan223360@gmail.com>
2025-06-10 14:06:44 +0700

    fix harusnya

--- webpack.common.js
+++ webpack.common.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: "/Project_FInal_Intermediate/",
     clean: true,
   },
   module: {
